ef9f83c04d801623a4f1e322f498e4e20c3408a9
Brett Seifried <seifriedbrett@gmail.com>
2021-11-29 15:51:52 -0800

    forgot to ACP often, completed deliverable

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import './Animal.css'
 
-export default function Animal() {
-  return <></>
+export default function Animal(props) {
+  // const animalClass = `${props.name} THE ${props.type} ${props.says} ${props.top} ${props.left}`
+
+  return (
+    <div className="animal" style={{ top: props.top, left: props.left }}>
+      <img alt={props.name} src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
+      <span className="name">{props.name}</span>
+      {props.name} {props.says}
+    </div>
+  )
 }
